5b9119e531b0702be1bb4cfe5cb652893a27cf66
TheDeadGPU <dsfreak0109@gmail.com>
2024-06-08 23:06:09 -0400

    Update moanalisa.py

--- moanalisa.py
+++ moanalisa.py
@@ -1,17 +1,17 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO Library
+import RPi.GPIO as GPIO
 import os
 import subprocess
 
-#Variables
+# Variables
 inputButtonPin = 17
-lastButtonState = 0 # 0 = Not Pressed 1 = Pressed
+lastButtonState = 0  # 0 = Not Pressed, 1 = Pressed
 
-#GPIO Setup
-GPIO.setwarnings(False) # Ignore warnings
-GPIO.setmode(GPIO.BCM) # Use GPIO Numbering
-GPIO.setup(inputButtonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Set the button pin to be an input pin and set initial value to be pulled low
+# GPIO Setup
+GPIO.setwarnings(False)  # Ignore warnings
+GPIO.setmode(GPIO.BCM)  # Use GPIO Numbering
+GPIO.setup(inputButtonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set the button pin to be an input pin and set initial value to be pulled low
 
-#Function that plays the moaning sound
+# Function that plays the moaning sound
 def play_sound(file_path):
     # Run the command in the background
     process = subprocess.Popen(['mpg321', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -26,13 +26,12 @@ def play_sound(file_path):
         print(f"The command has finished with an error code: {process.returncode}")
 
 def main():
-    while True: # Run forever
+    while True:  # Run forever
         buttonState = GPIO.input(inputButtonPin)
-        if(buttonState == GPIO.HIGH) and (buttonState != lastButtonState):
+        if buttonState == GPIO.HIGH and buttonState != lastButtonState:
             print("Button was pushed!")
-            #os.system("mpg321 Moaning.mp3 &")
             play_sound("Moaning.mp3")
             lastButtonState = buttonState
-            main()
-main()
 
+if __name__ == "__main__":
+    main()
